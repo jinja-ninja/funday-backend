37464bd1784bba0189f974e0f662e5dfa4b63f6c
galbarcessat <galbarcessat1@gmail.com>
2023-11-05 15:10:09 +0200

    app.use(express.json({limit: '50mb'})) added

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const server = http.createServer(app)
 
 // Express App Config
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
+// app.use(express.json())
 
 
 if (process.env.NODE_ENV === 'production') {

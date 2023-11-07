0e83f8e7297af3060c45195d4e34aa96768e521b
galbarcessat <galbarcessat1@gmail.com>
2023-11-07 11:45:31 +0200

    dev mode connected to Atlas instead of compass

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-  // dbURL: process.env.MONGO_URL || 'mongodb+srv://galbarcessat:galbarcessat123@cluster0.vsahcr7.mongodb.net/?retryWrites=true&w=majority',
-  dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://galbarcessat:galbarcessat123@cluster0.vsahcr7.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: 'mongodb://127.0.0.1:27017',
   dbName: 'funday_db'
 }
